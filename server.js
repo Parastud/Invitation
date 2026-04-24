b681d50e1e17a3880d3590e2f2526ebe15883309
Parastud <parthsharma07070@gmail.com>
2026-04-25 02:29:25 +0530

    push

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import path from 'path';
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
